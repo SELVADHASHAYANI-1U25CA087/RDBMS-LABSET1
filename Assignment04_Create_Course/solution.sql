50c26a48e49bae5db4cec75ea453bf5b651aa6ba
SELVADHASHAYANI-1U25CA087 <selvadhashayani7@gmail.com>
2026-08-25 09:54:00 +0530

    Revise SQL script for Course table creation
    Updated SQL script to create Course table and insert records.

--- Assignment04_Create_Course/solution.sql
+++ Assignment04_Create_Course/solution.sql
@@ -1,9 +1,10 @@
 DROP DATABASE IF EXISTS CollegeDB;
 CREATE DATABASE CollegeDB;
 USE CollegeDB;
-
--- Create Course table
-
--- Insert three records
-
--- Display structure
+USE collegeDBBCA;
+CREATE TABLE Course (courseID VARCHAR(20),credits INT(20) PRIMARY KEY,DepartmentID VARCHAR(20));
+INSERT INTO Course(courseID,courseName,credits,DepartmentID) VALUES (10,"BCA",12,1);
+INSERT INTO Course(courseID,courseName,credits,DepartmentID) VALUES (20,"BCA",13,1);
+INSERT INTO Course (courseID,courseName,credits,DepartmentID) VALUES (30,"BCA",14,1);
+SELECT * FROM Course;
+DESC Course;
