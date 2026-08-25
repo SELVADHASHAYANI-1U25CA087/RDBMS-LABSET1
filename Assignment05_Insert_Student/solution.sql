ec4c556cdcf2ce126353e34495da0b8a4b2621f9
SELVADHASHAYANI-1U25CA087 <selvadhashayani7@gmail.com>
2026-08-25 09:56:13 +0530

    Fix database and table names in SQL script
    Corrected database name and table name in INSERT statements.

--- Assignment05_Insert_Student/solution.sql
+++ Assignment05_Insert_Student/solution.sql
@@ -11,13 +11,9 @@ CREATE TABLE Student(
     Email VARCHAR(30),
     PhoneNumber BIGINT
 );
-
--- Insert
-
--- 1001 Arun
-
--- 1002 Divya
-
--- 1003 Karthik
-
--- Display all records
+USE CollegeDBBCA;
+INSERT INTO STUDENT1(StudentID,StudentName,Gender,DepartmentID) VALUES (1001,"Arun","Male",101);
+INSERT INTO STUDENT1(StudentID,StudentName,Gender,DepartmentID) VALUES(1002,"Divya","Female",102);
+INSERT INTO STUDENT1(StudentID,StudentName,Gender,DepartmentID) VALUES(1003,"Karthik","Male",103);
+SELECT*FROM STUDENT1;
+DESC STUDENT1;
