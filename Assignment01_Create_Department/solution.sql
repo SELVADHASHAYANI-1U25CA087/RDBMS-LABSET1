21cd7de459676047556456e33508ed8ecbd3515c
SELVADHASHAYANI-1U25CA087 <selvadhashayani7@gmail.com>
2026-08-27 10:59:01 +0530

    Restore SQL commands for database and table creation

--- Assignment01_Create_Department/solution.sql
+++ Assignment01_Create_Department/solution.sql
@@ -1,10 +1,4 @@
 CREATE DATABASE CollegeDB;
 USE CollegeDB;
 CREATE TABLE DEPARTMENT (Department_ID INT PRIMARY KEY,Department_Name VARCHAR(20), HOD VARCHAR (20));
--- Write your SQL here
 
--- Create Database
-
--- Use Database
-
--- Create Department Table
