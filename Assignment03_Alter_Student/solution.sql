1e00193fe50b73e12e7e82fe466c71152d8781da
SELVADHASHAYANI-1U25CA087 <selvadhashayani7@gmail.com>
2026-08-27 10:57:56 +0530

    Alter Student table to add Email and PhoneNumber
    Recreate the Student table and add Email and PhoneNumber columns.

--- Assignment03_Alter_Student/solution.sql
+++ Assignment03_Alter_Student/solution.sql
@@ -1,22 +1,4 @@
-DROP DATABASE IF EXISTS CollegeDB;
-CREATE DATABASE CollegeDB;
-USE CollegeDB;
-
-CREATE TABLE Student(
-    StudentID INT(5) PRIMARY KEY,
-    StudentName VARCHAR(20) NOT NULL,
-    DOB DATE,
-    Gender VARCHAR(10),
-    DepartmentID INT(5)
-);
 USE CollegeDBBCA;
 alter TABLE STUDENT1 ADD (Email VARCHAR (30),PhoneNumber INT(10));
 DESC STUDENT1;
 
--- Alter Student table
-
--- Add Email
-
--- Add PhoneNumber
-
--- Display structure
